f2e0b8ee87458d1597cc81852fb8311fa4aab21d
WhiteAzer <0202timur2020@gmail.com>
2023-02-25 16:40:55 +0300

    init: initial commit

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,17 @@
+import {WebpackPluginInstance, ProgressPlugin} from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { BuildPlugins } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export const buildPlugins = (paths: BuildPlugins): WebpackPluginInstance[] => {
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css"
+    })
+  ]
+}
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import "./styles/index.scss";
+
+
+export const App = () => {
+  return (<div/> );
+}
